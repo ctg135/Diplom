bb195be95a5f812e0cc7f56285c2f6e313fd763c
Programmer <programmer@c#.net>
2020-03-19 20:56:28 +0300

    added packages for log4net

---
+++ Client and Web-service for workers/Web-Service/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using log4net;
+using log4net.Config;
+
+namespace Web_Service
+{
+    public static class Logger
+    {
+        public static ILog Log { get; } = LogManager.GetLogger("LOGGER");
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+    }
+}
