6ac7f9f11923cb6319b6aa43e2d67dc79883571a
Programmer <programmer@c#.net>
2020-03-20 20:56:52 +0300

    Added testter

---
+++ Client and Web-service for workers/Testter/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using Newtonsoft.Json;
+using Web_Service.Models;
+
+namespace Testter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch(ModelSelector.Text)
+            {
+                case "Autho":
+                    Model.Text = JsonConvert.SerializeObject(new Autho());
+                    break;
+            }
+        }
+
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpRequestMessage request = new HttpRequestMessage();
+
+            request.RequestUri = new Uri(Address.Text);
+            request.Headers.Add("Accept", "application/json");
+
+            switch (Method.Text)
+            {
+                case "GET":
+                    request.Method = HttpMethod.Get;
+                    break;
+                case "PUT":
+                    request.Method = HttpMethod.Put;
+                    request.Content = new StringContent(RequestContent.Text);
+                    break;
+                case "POST":
+                    request.Method = HttpMethod.Post;
+                    request.Content = new StringContent(RequestContent.Text);
+                    break;
+                case "DELETE":
+                    request.Method = HttpMethod.Delete;
+                    request.Content = new StringContent(RequestContent.Text);
+                    break;
+            }
+
+            string temp1 = "";
+            foreach (var header in request.Headers)
+            {
+                temp1 += $"# {header.Key}:\n";
+                List<string> vals = new List<string>(header.Value);
+                foreach (var val in vals)
+                {
+                    temp1 += $" # {val}\n";
+                }
+            }
+            RequestHeaders.Text = temp1;
+
+            HttpResponseMessage responseMessage = await client.SendAsync(request);
+
+            ResponseContent.Text = responseMessage.Content.ReadAsStringAsync().Result;
+
+            string temp = "H E A D E R S\n";
+            foreach(var header in responseMessage.Headers)
+            {
+                temp += $"# {header.Key}:\n";
+                List<string> vals = new List<string>(header.Value);
+                foreach (var val in vals)
+                {
+                    temp += $" * {val}\n";
+                }
+            }
+            temp += "C O N T E N T  H E A D E R S\n";
+            foreach(var header in responseMessage.Content.Headers)
+            {
+                temp += $"# {header.Key}:\n";
+                List<string> vals = new List<string>(header.Value);
+                foreach (var val in vals)
+                {
+                    temp += $" * {val}\n";
+                }
+            }
+            ResponseHeaders.Text = temp;
+        }
+    }
+}
--- Client and Web-service for workers/Web-Service/Controllers/ConnectionController.cs
+++ Client and Web-service for workers/Web-Service/Controllers/ConnectionController.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Web_Service.DataBase;
-
+using System.Threading.Tasks;
 using System.Data;
 using System.Text;
 using System.Security.Cryptography;
@@ -19,11 +19,12 @@ namespace Web_Service.Controllers
         /// <code>GET: api/Connection</code>
         /// Производит проверку подключения к серверу
         /// </summary>
-        public HttpResponseMessage Get(HttpRequestMessage message)
+        public async Task<HttpResponseMessage> Get(HttpRequestMessage message)
         {
             Logger.Log.Debug("api/Connection GET Получено сообщение");
             Logger.Log.Info("api/Connection GET Сообщение обработано");
-            return new HttpResponseMessage() { StatusCode = HttpStatusCode.OK, Content = new StringContent("Ok") };
+
+            return new HttpResponseMessage() { StatusCode = HttpStatusCode.OK, Content = new StringContent(await message.Content.ReadAsStringAsync()) };
         }
     }
 }
