7781baee5592e8d1a05dfc19de0cc2ccf0c95601
Programmer <programmer@c#.net>
2020-03-31 11:31:53 +0300

    Added statuses and fixed name of columns

--- База данных/workers.sql
+++ База данных/workers.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Мар 20 2020 г., 19:27
+-- Время создания: Мар 31 2020 г., 12:23
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -49,9 +49,10 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   `Token` varchar(40) NOT NULL,
   `ClientInfo` text NOT NULL,
   `Created` datetime NOT NULL,
+  `LastUpdate` datetime NOT NULL,
   PRIMARY KEY (`Id`),
   KEY `WorkerId` (`WorkerId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -64,7 +65,20 @@ CREATE TABLE IF NOT EXISTS `statuses` (
   `Title` varchar(30) DEFAULT NULL,
   `Description` text,
   PRIMARY KEY (`Code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+--
+-- Дамп данных таблицы `statuses`
+--
+
+INSERT INTO `statuses` (`Code`, `Title`, `Description`) VALUES
+(1, 'Не установлен', 'Статус не был установлен (рабочий день не начался)'),
+(2, 'На работе', 'Работник на рабочем месте'),
+(3, 'На перерыве', 'Работник находится на перерыве'),
+(4, 'В отпуске', 'В отпуске'),
+(5, 'Рабочий день закончен', 'Работник отрабтал весь день'),
+(6, 'На больничном', 'На больничном'),
+(7, 'Командировка', 'В командировке');
 
 -- --------------------------------------------------------
 
@@ -81,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `statuslogs` (
   PRIMARY KEY (`Id`),
   KEY `WorkerId` (`WorkerId`,`StatusCode`),
   KEY `StatusCode` (`StatusCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -92,18 +106,18 @@ CREATE TABLE IF NOT EXISTS `statuslogs` (
 CREATE TABLE IF NOT EXISTS `workers` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(30) NOT NULL,
-  `Surname` varchar(30) DEFAULT NULL,
-  `Patronymic` varchar(30) DEFAULT NULL,
-  `BirthDate` date DEFAULT NULL,
+  `Surname` varchar(30) NOT NULL,
+  `Patronymic` varchar(30) NOT NULL,
+  `BirthDate` date NOT NULL,
   `Mail` varchar(60) DEFAULT NULL,
-  `Position` varchar(30) DEFAULT NULL,
-  `Rate` int(11) DEFAULT NULL,
-  `AccessLevel` int(11) DEFAULT NULL,
-  `Login` varchar(40) NOT NULL,
-  `Password` varchar(40) NOT NULL,
+  `Position` varchar(30) NOT NULL,
+  `Rate` int(11) NOT NULL,
+  `AccessLevel` int(11) NOT NULL,
+  `Login` varchar(30) NOT NULL,
+  `Password` varchar(30) NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Login` (`Login`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
