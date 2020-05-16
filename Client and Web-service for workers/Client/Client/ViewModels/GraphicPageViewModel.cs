dc1711153b7ebace9576fbb11a7ee4f98f7ca243
Programmer <programmer@c#.net>
2020-05-16 20:31:47 +0300

    Added view and saving plans to GraphicPage

--- Client and Web-service for workers/Client/Client/ViewModels/GraphicPageViewModel.cs
+++ Client and Web-service for workers/Client/Client/ViewModels/GraphicPageViewModel.cs
@@ -21,6 +21,11 @@ namespace Client.ViewModels
         /// </summary>
         public IClientModel Client { get; private set; }
         /// <summary>
+        /// Загрузчик сохраненных планов
+        /// </summary>
+        public IPlanLoader PlanLoader { get; private set; }
+        #region xamlData
+        /// <summary>
         /// Начальная дата для отбора
         /// </summary>
         public DateTime StartDate { get; set; }
@@ -32,6 +37,14 @@ namespace Client.ViewModels
         /// Команда для загрузки планов
         /// </summary>
         public ICommand ShowPlans { get; private set; }
+        /// <summary>
+        /// Команда для сохранения планов
+        /// </summary>
+        public ICommand SavePlans { get; private set; }
+        /// <summary>
+        /// Команда просмотра сохраненных планов
+        /// </summary>
+        public ICommand OpenSavedPlans { get; private set; }
         /// <summary>
         /// Значение выбранного рабочего дня в фильтрах
         /// </summary>
@@ -48,6 +61,7 @@ namespace Client.ViewModels
         /// Значение выбранного больничного в фильтрах
         /// </summary>
         public bool HospitalDaysSelected { get; set; }
+        #endregion
         /// <summary>
         /// Событие просмотра графиков
         /// </summary>
@@ -56,13 +70,17 @@ namespace Client.ViewModels
         /// Создание модели
         /// </summary>
         /// <param name="Client"></param>
-        public GraphicPageViewModel(IClientModel Client)
+        public GraphicPageViewModel(IClientModel Client, IPlanLoader PlanLoader)
         {
             ShowPlans = new Command(LoadPlans);
+            SavePlans = new Command(SaveSelectedPlans);
+            OpenSavedPlans = new Command(OpenSaved);
 
             this.Client = Client;
             this.Client.Session = Globals.Config.GetItem("Session").Result;
             this.Client.Server = Globals.Config.GetItem("Server").Result;
+
+            this.PlanLoader = PlanLoader;
 
             StartDate = EndDate = DateTime.Parse( DateTime.Now.ToString("d") );
         }
@@ -95,6 +113,51 @@ namespace Client.ViewModels
                 this.ViewPlans(this, new ViewPlansEventArgs(Plans));
             }
             catch(Exception exc)
+            {
+                await FatalError(exc.Message);
+                return;
+            }
+        }
+        private async void SaveSelectedPlans(object param)
+        {
+            var Plans = new List<Plan1>();
+            if (StartDate > EndDate)
+            {
+                await Application.Current.MainPage.DisplayAlert("Ошибка ввода", "Начальная дата не может быть больше конечной", "Ок");
+                return;
+            }
+
+            List<PlanTypes> filter = new List<PlanTypes>();
+            if (WorkDaysSelected) filter.Add(PlanTypes.Working);
+            if (DaysOffSelected) filter.Add(PlanTypes.DayOff);
+            if (HolidayDaysSelected) filter.Add(PlanTypes.Holiday);
+            if (HospitalDaysSelected) filter.Add(PlanTypes.Hospital);
+
+            try
+            {
+                Plans = await Client.GetPlans(StartDate, EndDate, filter.ToArray());
+                foreach (var plan in Plans)
+                {
+                    plan.TypePlan = Globals.PlanTypes[plan.TypePlan];
+                }
+                await PlanLoader.SetPlans(Plans);
+                await Application.Current.MainPage.DisplayAlert("Все готово!", "Планы сохранены в памяти", "Ок");
+            }
+            catch (Exception exc)
+            {
+                await FatalError(exc.Message);
+                return;
+            }
+        }
+        private async void OpenSaved(object param)
+        {
+            try
+            {
+                var Plans = await PlanLoader.GetPlans();
+                System.Diagnostics.Debug.WriteLine("Выводим сохр графики");
+                this.ViewPlans(this, new ViewPlansEventArgs(Plans));
+            }
+            catch (Exception exc)
             {
                 await FatalError(exc.Message);
                 return;
